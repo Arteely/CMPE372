aa696b1e00d7b792e64135bff25770d59a4678f6
Artem Artemyev <artemyevart@gmail.com>
2022-05-27 10:37:28 +0300

    pain

--- StudentMngmtProject/config.php
+++ StudentMngmtProject/config.php
@@ -3,6 +3,12 @@
 $link = mysqli_connect('localhost', 'root', '', 'casara_sis');
  
 $name = "";
+$surname = "";
+$username = "";
+$password = "";
+$password_confirmation = "";
+$faculty_id = "";
+
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
