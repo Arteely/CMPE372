d809dce304fb12d38863fbbb73add6d0d56949ff
Artem Artemyev <artemyevart@gmail.com>
2022-06-08 16:05:11 +0300

    added course page

---
+++ StudentMngmtProject/pages/courses.php
@@ -0,0 +1,57 @@
+<div class="main-content-body">
+    <div class="course-container">
+        <div class="course-intro">
+            <span class="greet-title-text">BUS274: Further Statistics for Business</span>
+            <span class="greet-subheading">Haluk Levent, Faculty of Business</span>
+        </div>
+        <div class="course-banner-container">
+            <img class="course-banner" src="https://images.pexels.com/photos/669616/pexels-photo-669616.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2">
+        </div>
+        <div class="course-description">
+            <div class="tabset">
+                <!-- Tab 1 -->
+                <input type="radio" name="tab-button" id="tab-button-1" aria-controls="notes" checked>
+                <label for="tab-button-1">Course Notes</label>
+                <!-- Tab 2 -->
+                <input type="radio" name="tab-button" id="tab-button-2" aria-controls="resources">                    <label for="tab-button-2">Resources</label>
+                <!-- Tab 3 -->
+                <input type="radio" name="tab-button" id="tab-button-3" aria-controls="exams">
+                <label for="tab-button-3">Exams</label>
+  
+                <div class="tab-panels">
+                    <section id="notes" class="tab-panel">
+                        <h3>BUS 274 - Further Statistics for Business</h3>
+                        <p>Further development of the material in BUS 273, in particular the analysis of variance, 
+                        multiple regression, non-parametric procedures and the analysis of categorical data. 
+                        Data analysis via statistical packages.
+                        </p>
+                    </section>
+                    <section id="resources" class="tab-panel">
+                        <h3>6B. Rauchbier</h3>
+                        <p><strong>Overall Impression:</strong>  An elegant, malty German amber lager with a balanced, complementary beechwood smoke character. Toasty-rich malt in aroma and flavor, restrained bitterness, low to high smoke flavor, clean fermentation profile, and an attenuated finish are characteristic.</p>
+                        <p><strong>History:</strong> A historical specialty of the city of Bamberg, in the Franconian region of Bavaria in Germany. Beechwood-smoked malt is used to make a Märzen-style amber lager. The smoke character of the malt varies by maltster; some breweries produce their own smoked malt (rauchmalz).</p>
+                    </section>
+                    <section id="exams" class="tab-panel">
+                        <h3>6C. Dunkles Bock</h3>
+                        <p><strong>Overall Impression:</strong> A dark, strong, malty German lager beer that emphasizes the malty-rich and somewhat toasty qualities of continental malts without being sweet in the finish.</p>
+                        <p><strong>History:</strong> Originated in the Northern German city of Einbeck, which was a brewing center and popular exporter in the days of the Hanseatic League (14th to 17th century). Recreated in Munich starting in the 17th century. The name “bock” is based on a corruption of the name “Einbeck” in the Bavarian dialect, and was thus only used after the beer came to Munich. “Bock” also means “Ram” in German, and is often used in logos and advertisements.</p>
+                    </section>
+                </div>
+            </div>
+            <div class="instructor-description">
+                <h3>About Instructor</h3>
+                <div class="instructor-profile">
+                    <img class="instructor-image" src="assets/profile-picture-selfmade.png">
+                    <div class="instructor-info">
+                        <span class="instructor-name">Haluk Levent</span><br>
+                        <span class="instructor-faculty">Faculty of Business</span>
+                    </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta asperiores provident voluptates ipsum eum deleniti nostrum quisquam, officia alias vero a unde ad possimus architecto culpa laboriosam! Sit, atque nam!</p>
+            </div>
+        </div>
+    </div>
+    <div class="course-tasks-container">
+        
+    </div>
+</div>
